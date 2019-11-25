e7be14592380ea1c27317193e5b09a18edc3a7fd
sleekice7 <38444870+sleekice7@users.noreply.github.com>
2019-11-25 16:38:59 +0000

    Add files via upload

---
+++ sec_module2/services.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#---MOP WORK PACKAGE TAG: WP021V3---
+#---DESC:
+#---THIS SCRIPT WILL DISABLE UNSECURED SERVICES---
+#---SCRIPT BY REUBEN A. BOAKYE
+#SIGNUM: EBOAREU---
+
+#---MAIN CONFIG COMMAND OBTAINED FROM NODE HARDENING CPI---
+
+#---All scripts run are logged into a single log file called "scripts.log"
+#with file path /var/log/script.log---
+
+timeStamp=$(date '+%m_%y_%H_%M_%S')
+logName="script"
+fExtension=".log"
+logFile=/var/log/$logName$fExtension
+
+sudo chkconfig --list
+echo "Please provide the name of the service you wish to turn off, from among the list displayed above:"
+read source
+sudo chkconfig $source off
+STATUS=`echo "$?"`
+if test -f "$logFile"; then 
+	if [ $STATUS -eq 0 ]; then
+		sudo echo "$USER $timeStamp Exit_Status: Successfully Executed" >> $logFile
+	else 			
+		sudo echo "$USER $timeStamp Exit_Status `./services.sh`" >> $logFile
+	fi
+else
+	sudo touch $logFile
+	if [ $STATUS -eq 0 ]; then
+		sudo echo "$USER $timeStamp Exit_Status: Successfully Executed" >> $logFile
+	else
+		sudo echo "$USER $timeStamp Exit_Status: Failed to Execute: `./services.sh`" >> $logFile
+	fi
+fi
---
+++ sec_module2/sshtimeout.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#---MOP WORK PACKAGE TAG: WP021BISV3---
+#---DESC:
+#---CONFIGURING SSH SESSION TIMEOUT---
+
+#---SCRIPT BY REUBEN A. BOAKYE
+#SIGNUM: EBOAREU---
+
+#---MAIN CONFIG COMMAND OBTAINED FROM NODE HARDENING CPI---
+
+#---All scripts run are logged into a single log file called "script.log"
+#with file path /var/log/script.log---
+
+
+
+
+
+echo "Please enter your ID and address of primary IO to be logged onto the primary IO as tspSecAdmin user:"
+
+echo "ID:"
+read value
+
+echo "Address:"
+read address
+
+sudo ssh $value@$address
+
+echo "NOTE: PLEASE ACT ACCORDING TO THE PRINTOUT OF THE SCRIPT TO ACTUALIZE THE SETTING"
+echo "Please provide the desired timeout threshold in seconds:"
+read time
+
+sudo set_session_timeout.sh -t \ $time
+
+
+echo "Please enter your ID and address of secondary IO to be logged onto the primary IO as tspSecAdmin user:"
+
+echo "ID:"
+read value
+
+echo "Address:"
+read address
+
+sudo ssh $value@$address
+
+echo "NOTE: PLEASE ACT ACCORDING TO THE PRINTOUT OF THE SCRIPT TO ACTUALIZE THE SETTING"
+echo "Please provide the desired timeout threshold in seconds:"
+read time
+
+sudo set_session_timeout.sh -t \ $time
+
+
+
+
+
