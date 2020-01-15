777a6e79952de4dfbe33e057dab6307961ce974d
sleekice7 <38444870+sleekice7@users.noreply.github.com>
2020-01-15 17:34:56 -0500

    Update sc_validation2.sh

--- sec_module6/sc_validation2.sh
+++ sec_module6/sc_validation2.sh
@@ -6,45 +6,41 @@
 
 #---SCRIPT BY REUBEN A BOAKYE
 #SIGNUM: EBOAREU---
-
-#---PRINT PRESENT WORKING DIRECTORY---
-pwd     
+   
 
 #---CHECK IF GNOME-SCREENSHOT PACKAGE IS INSTALLED---
-#---THIS IS PACKAGE NEEDED FOR THE SCREENSHOT TO BE TAKEN---
-filename="gnome-screenshot" 
-if rpm -qa | grep -i $filename
-then
-	echo "`tput bold`Package is already installed"
-else 
-	echo "`tput bold`Package not installed. Proceeding to install..."	
-	sudo yum -y install $filename
-fi  
+#---THIS IS THE PACKAGE NEEDED FOR THE SCREENSHOT TO BE TAKEN---
 
-#---/ETC/ FILES---
+sc_validation2 () {
+	filename="gnome-screenshot" 
+	if rpm -qa | grep -i $filename
+	then
+		echo "`tput bold`Package is already installed"
+	else 
+		echo "`tput bold`Package not installed. Proceeding to install..."	
+		sudo yum -y install $filename
+	fi  
 
-nano /etc/passwd
+	#---/ETC/ FILES---
 
-name=$(date '+%m_%y_%H_%M_%S')
-imgExtension=".png"
-imgName=$name$imgExtension
-#imgDirectory="/Pictures/"
-#imgSavePath=$imgDirectory$imgName
-gnome-screenshot -w --file="$imgName"
+	nano /etc/passwd
 
-nano /etc/shadow
+	name=$(date '+%m_%y_%H_%M_%S')
+	imgExtension=".png"
+	imgName=$name$imgExtension
+	#imgDirectory="/Pictures/"
+	#imgSavePath=$imgDirectory$imgName
+	gnome-screenshot -w --file="$imgName"
 
-name=$(date '+%m_%y_%H_%M_%S')
-imgExtension=".png"
-imgName=$name$imgExtension
-#imgDirectory="/Pictures/"
-#imgSavePath=$imgDirectory$imgName
-gnome-screenshot -w --file="$imgName"
+	nano /etc/shadow
 
+	name=$(date '+%m_%y_%H_%M_%S')
+	imgExtension=".png"
+	imgName=$name$imgExtension
+	#imgDirectory="/Pictures/"
+	#imgSavePath=$imgDirectory$imgName
+	gnome-screenshot -w --file="$imgName"
+}
 
-
-  
-#BELOW IS FOR USE IF TIMESTAMP HAS TO BE IMPLEMENTED AS FILENAME.
-#currentDate=$(date '+DATE:%m_%y_TIME:%H_M_S')
-
+sc_validation2
 
