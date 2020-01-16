a308e6474f28fdc7b348db407881f9c26f851f67
sleekice7 <38444870+sleekice7@users.noreply.github.com>
2020-01-15 19:33:29 -0500

    Update sshtimeout.sh

--- sec_module2/sshtimeout.sh
+++ sec_module2/sshtimeout.sh
@@ -4,51 +4,48 @@
 #---DESC:
 #---CONFIGURING SSH SESSION TIMEOUT---
 
-#---SCRIPT BY REUBEN A. BOAKYE
-#SIGNUM: EBOAREU---
-
 #---MAIN CONFIG COMMAND OBTAINED FROM NODE HARDENING CPI---
 
 #---All scripts run are logged into a single log file called "script.log"
 #with file path /var/log/script.log---
 
 
+sshtimeout () {
+  echo "Please enter your ID and address of primary IO to be logged onto the primary IO as tspSecAdmin user:"
+
+  echo "ID:"
+  read value
+
+  echo "Address:"
+  read address
+
+  sudo ssh $value@$address
+
+  echo "NOTE: PLEASE ACT ACCORDING TO THE PRINTOUT OF THE SCRIPT TO ACTUALIZE THE SETTING"
+  echo "Please provide the desired timeout threshold in seconds:"
+  read time
+
+  sudo set_session_timeout.sh -t \ $time
 
 
+  echo "Please enter your ID and address of secondary IO to be logged onto the primary IO as tspSecAdmin user:"
 
-echo "Please enter your ID and address of primary IO to be logged onto the primary IO as tspSecAdmin user:"
+  echo "ID:"
+  read value
 
-echo "ID:"
-read value
+  echo "Address:"
+  read address
 
-echo "Address:"
-read address
+  sudo ssh $value@$address
 
-sudo ssh $value@$address
+  echo "NOTE: PLEASE ACT ACCORDING TO THE PRINTOUT OF THE SCRIPT TO ACTUALIZE THE SETTING"
+  echo "Please provide the desired timeout threshold in seconds:"
+  read time
 
-echo "NOTE: PLEASE ACT ACCORDING TO THE PRINTOUT OF THE SCRIPT TO ACTUALIZE THE SETTING"
-echo "Please provide the desired timeout threshold in seconds:"
-read time
+  sudo set_session_timeout.sh -t \ $time
+}
 
-sudo set_session_timeout.sh -t \ $time
-
-
-echo "Please enter your ID and address of secondary IO to be logged onto the primary IO as tspSecAdmin user:"
-
-echo "ID:"
-read value
-
-echo "Address:"
-read address
-
-sudo ssh $value@$address
-
-echo "NOTE: PLEASE ACT ACCORDING TO THE PRINTOUT OF THE SCRIPT TO ACTUALIZE THE SETTING"
-echo "Please provide the desired timeout threshold in seconds:"
-read time
-
-sudo set_session_timeout.sh -t \ $time
-
+sshtimeout
 
 
 
