476eb29b9a0fb2bdbe9aa570b5a4c6f02a13dd74
sleekice7 <38444870+sleekice7@users.noreply.github.com>
2020-01-16 06:17:35 -0500

    Update auditlog1a.sh

--- sec_module3/auditlog1a.sh
+++ sec_module3/auditlog1a.sh
@@ -2,34 +2,25 @@
 
 #---MOP WORK PACKAGE TAG: WP035V3---
 #---DESC:
-#---CONFIGURING REMOTE SYSLOG FROM UNIX/LINUX SERVER---
+#---ENABLE AUDIT LOGGING---
 
 #---SCRIPT BY REUBEN A. BOAKYE---
 #---SIGNUM: EBOAREU---
 
-#---MAIN CONFIGURATION COMMAND OBTAINED FROM NODE HARDENING CPI---
 
 #---All scripts run are logged into a single log file called "script.log"
 #with file path /var/log/script.log---
 
-auditlog1a () {
-	file=/etc/rsyslog.conf
-	fileNew=/etc/audisp/plugins.d/syslog.conf
+auditlog1 () {
 
-	#Set up remote logging
-	if test -f "$file"; then
 		timeStamp=$(date '+%m_%y_%H_%M_%S')
-		fExtension=".conf"
 		fExtension1=".log"
-		backupFile=/etc/$timeStamp$fExtension
 		logName="script"
 		logFile=/var/log/$logName$fExtension1
 
-	#This creats a backup of the configuration file.
-		sudo cp $file $backupFile
-
-	#This command will configure remote log server.
-		sudo lecfengine module-commit -m EHardeningSetup -i
+	#This command will output the status of audit logging, whether enabled or not.
+		sudo auditctl -s
+		sudo autditctl -e 1
 
 		STATUS=`echo "$?"`
 
@@ -37,53 +28,35 @@ auditlog1a () {
 		if test -f "$logFile"; then 
 			if [ $STATUS -eq 0 ]; then
 				echo "$USER $timeStamp Exit_Status: Successfully Executed" >> $logFile
+				systemctl restart auditd
 			else
 				echo "$USER $timeStamp Exit_Status: Failed to Execute." >> $logFile
-				mv $backupfile $file
 			fi
 		else
 			sudo touch $logFile
 			if [ $STATUS -eq 0 ]; then
 				echo "$USER $timeStamp Exit_Status: Successfully Executed" >> $logFile
+				systemctl restart auditd
 			else
 				echo "$USER $timeStamp Exit_Status: Failed to Execute." >> $logFile
-				sudo mv $backupfile $file
 			fi
 		fi
-	else 
-		echo "***THE CONFIGURATION FILE DOES NOT EXIST!***"
-	fi
-
-	#Enable remote audit logging.
-	if test -f "$fileNew"; then
-		timeStamp=$(date '+%m_%y_%H_%M_%S')
-		fExtension=".conf"
-		fExtension1=".log"
-		backupFile=/etc/audisp/plugins.d/$timeStamp$fExtension
-		logFile=/etc/audisp/plugins.d/script.log
-
-	#This creats a backup of the configuration file.
-		sudo cp $fileNew $backupFile	
-
-		sed -i "s/active/active = yes/g" $fileNew
-		sed -i "s/direction/direction = out/g" $fileNew
-		sed -i "s/path/path = builtin_syslog/g" $fileNew
-		sed -i "s/type/type = builtin/g" $fileNew
-		sed -i "s/args/args = LOG_INFO/g" $fileNew
-		sed -i "s/format/format = string/g" $fileNew
-
+		
+		
+		
+	#This command lists all loaded audit rules.
+		echo "########## PLEASE VERIFY THE FOLLOWING AUDIT RULES #########"
+		sudo auditctl -l
+		
 		STATUS=`echo "$?"`
+		
 		if [ $STATUS -eq 0 ]; then
 			echo "$USER $timeStamp Exit_Status: Successfully Executed" >> $logFile
-			sudo service rsyslog restart
 		else
 			echo "$USER $timeStamp Exit_Status: Failed to Execute." >> $logFile
-			sudo mv $backupfile $fileNew
 		fi
-	else 
-		echo "***THE CONFIGURATION FILE DOES NOT EXIST!***"
-	fi
+
 }
 
-auditlog1a
+auditlog1
 	
