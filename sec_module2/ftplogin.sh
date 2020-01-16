479a1c39579ff397143bc60955f171748fb7f466
sleekice7 <38444870+sleekice7@users.noreply.github.com>
2020-01-15 19:26:43 -0500

    Update ftplogin.sh

--- sec_module2/ftplogin.sh
+++ sec_module2/ftplogin.sh
@@ -4,47 +4,51 @@
 #---DESC:
 #---DISABLING ANONYMOUS FTP LOGIN---
 
-#---SCRIPT BY REUBEN A. BOAKYE
-#SIGNUM: EBOAREU---
+#---SCRIPT BY REUBEN A. BOAKYE---
+#---SIGNUM: EBOAREU---
 
 #---All scripts run are logged into a single log file called "script.log"
 #with file path /var/log/script.log---
 
-file=/etc/vsftpd/vsftpd.conf
+ftplogin () {
+	file=/etc/vsftpd/vsftpd.conf
 
-if test -f "$file"; then
-	timeStamp=$(date '+%m_%y_%H_%M_%S')
-	fExtension=".conf"
-	fExtension1=".log"
-	backupFile=/etc/vsftpd/$timeStamp$fExtension
-	logName="script"
-	logFile=/var/log/$logName$fExtension1
-	
-#This creates a backup of the configuration file.
-	sudo cp $file $backupFile
-	sed -i "s/.*anonymous_enable.*/anonymous_enable=NO/g" $file
+	if test -f "$file"; then
+		timeStamp=$(date '+%m_%y_%H_%M_%S')
+		fExtension=".conf"
+		fExtension1=".log"
+		backupFile=/etc/vsftpd/$timeStamp$fExtension
+		logName="script"
+		logFile=/var/log/$logName$fExtension1
 
-	STATUS= echo "$?"
+	#This creates a backup of the configuration file.
+		sudo cp $file $backupFile
+		sed -i "s/.*anonymous_enable.*/anonymous_enable=NO/g" $file
 
-#This tests if logfile is already created.
-	if test -f "$logFile"; then 
-		if [ "$STATUS" -eq 0 ]; then
-			sudo echo "$USER $timeStamp Exit_Status: Successfully Executed" >> $logFile
+		STATUS= echo "$?"
+
+	#This tests if logfile is already created.
+		if test -f "$logFile"; then 
+			if [ "$STATUS" -eq 0 ]; then
+				sudo echo "$USER $timeStamp Exit_Status: Successfully Executed" >> $logFile
+			else
+				sudo echo "$USER $timeStamp Exit_Status: Failed to Execute." >> $logFile
+				sudo mv $backupfile $file
+			fi
 		else
-			sudo echo "$USER $timeStamp Exit_Status: Failed to Execute." >> $logFile
-			sudo mv $backupfile $file
+			sudo touch $logFile
+			if [ "$STATUS" -eq 0 ]; then
+				sudo echo "$USER $timeStamp Exit_Status: Successfully Executed" >> $logFile
+			else
+				sudo echo "$USER $timeStamp Exit_Status: Failed to Execute." >> $logFile
+				sudo mv $backupfile $file
+			fi
 		fi
-	else
-		sudo touch $logFile
-		if [ "$STATUS" -eq 0 ]; then
-			sudo echo "$USER $timeStamp Exit_Status: Successfully Executed" >> $logFile
-		else
-			sudo echo "$USER $timeStamp Exit_Status: Failed to Execute." >> $logFile
-			sudo mv $backupfile $file
-		fi
+
+		sudo service vsftpd restart
+	else 
+		echo "***THE CONFIGURATION FILE DOES NOT EXIST!***"
 	fi
-	
-	sudo service vsftpd restart
-else 
-	echo "***THE CONFIGURATION FILE DOES NOT EXIST!***"
-fi
+}
+
+ftplogin
