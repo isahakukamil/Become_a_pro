ea6057006faa6c1046ae3a4a02dd964362eadb38
sleekice7 <38444870+sleekice7@users.noreply.github.com>
2020-01-15 19:35:26 -0500

    Update sshtimeout1.sh

--- sec_module2/sshtimeout1.sh
+++ sec_module2/sshtimeout1.sh
@@ -7,53 +7,54 @@
 #---SCRIPT BY REUBEN A. BOAKYE
 #SIGNUM: EBOAREU---
 
-#---MAIN CONFIG COMMAND OBTAINED FROM NODE HARDENING CPI---
-
 #---All scripts run are logged into a single log file called "script.log"
 #with file path /var/log/script.log---
 
-file=/etc/ssh/sshd_config
+sshtimeout1 () {
+	file=/etc/ssh/sshd_config
 
-if test -f "$file"; then
-	timeStamp=$(date '+%m_%y_%H_%M_%S')
-	fExtension1=".log"
-	backupFile=/etc/ssh/ssh_config_$timeStamp
-	logName="script"
-	logFile=/var/log/$logName$fExtension1
+	if test -f "$file"; then
+		timeStamp=$(date '+%m_%y_%H_%M_%S')
+		fExtension1=".log"
+		backupFile=/etc/ssh/ssh_config_$timeStamp
+		logName="script"
+		logFile=/var/log/$logName$fExtension1
 
-#This creates a backup of the configuration file.
-	sudo cp $file $backupFile
-	
-	echo "Please enter the desired timeout parameters to effect changes"
-	echo "ClientAliveInterval:"
-	read interval
-	echo "ClientAliveCountMax:"
-	read count
+	#This creates a backup of the configuration file.
+		sudo cp $file $backupFile
 
-	sudo sed -i 's/.*ClientAliveInterval.*/ClientAliveInterval $interval/g' $file
-	sudo sed -i 's/.*ClientAliveCountMax.*/#ClientAliveCountMax $count/g' $file
+		echo "Please enter the desired timeout parameters to effect changes"
+		echo "ClientAliveInterval:"
+		read interval
+		echo "ClientAliveCountMax:"
+		read count
 
-	STATUS=`echo "$?"`
-	
-#This tests if logfile is already created.
-	if test -f "$logFile"; then 
-		if [ "$STATUS" -eq 0 ]; then
-			sudo echo "$USER $timeStamp Exit_Status: Successfully Executed" >> $logFile
+		sudo sed -i 's/.*ClientAliveInterval.*/ClientAliveInterval $interval/g' $file
+		sudo sed -i 's/.*ClientAliveCountMax.*/#ClientAliveCountMax $count/g' $file
+
+		STATUS=`echo "$?"`
+
+	#This tests if logfile is already created.
+		if test -f "$logFile"; then 
+			if [ "$STATUS" -eq 0 ]; then
+				sudo echo "$USER $timeStamp Exit_Status: Successfully Executed" >> $logFile
+			else
+				sudo echo "$USER $timeStamp Exit_Status: Failed to Execute." >> $logFile
+				sudo mv $backupfile $file
+			fi
 		else
-			sudo echo "$USER $timeStamp Exit_Status: Failed to Execute." >> $logFile
-			sudo mv $backupfile $file
+			sudo touch $logFile
+			if [ "$STATUS" -eq 0 ]; then
+				sudo echo "$USER $timeStamp Exit_Status: Successfully Executed" >> $logFile
+			else
+				sudo echo "$USER $timeStamp Exit_Status: Failed to Execute." >> $logFile
+				sudo mv $backupfile $file
+			fi
 		fi
-	else
-		sudo touch $logFile
-		if [ "$STATUS" -eq 0 ]; then
-			sudo echo "$USER $timeStamp Exit_Status: Successfully Executed" >> $logFile
-		else
-			sudo echo "$USER $timeStamp Exit_Status: Failed to Execute." >> $logFile
-			sudo mv $backupfile $file
-		fi
+		sudo service sshd restart
+	else 
+		echo "***THE CONFIGURATION FILE DOES NOT EXIST!***"
 	fi
-	sudo service sshd restart
-else 
-	echo "***THE CONFIGURATION FILE DOES NOT EXIST!***"
-fi
+}
 
+sshtimeout1
